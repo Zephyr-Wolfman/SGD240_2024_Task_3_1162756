88fa801ad10338348f4dd44af5f3171f5de41f45
Zephyr-Wolfman <n_j038@student.usc.edu.au>
2024-08-31 15:28:47 +1000

    Further Developed the AgentNavMesh Script

---
+++ Assets/Script/PatrolWaypoints.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PatrolWaypoints : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        foreach (Transform t in transform)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(t.position, 0.5f);
+        }
+    }
+}
